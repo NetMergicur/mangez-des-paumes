571a188f060e77522fd4bbc14b7ecdcff3e73805
AdrieanKhisbe <adriean.khisbe@live.fr>
2016-01-17 00:04:13 +0100

    init: add inde and test skeleton

---
+++ test/index.js
@@ -0,0 +1,15 @@
+const Lab = require('lab')
+const Code = require('code')
+
+const lab = exports.lab = Lab.script()
+const describe = lab.describe
+const it = lab.it
+const expect = Code.expect
+
+describe('mangez-des-trolls', () => {
+
+    it('would need some more tests', (done) => {
+        done()
+    })
+
+})
