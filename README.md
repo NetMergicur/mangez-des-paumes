b6fd47b06f45e2d586120fdaf73b67304fd1b3a3
AdrieanKhisbe <adriean.khisbe@live.fr>
2016-01-17 00:20:45 +0100

    init: replace with twit, and skeleton for index

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Trolleur Cibleur pour Twiteur
 # A propos
 
 Basé sur [APi de streaming de twitter](https://dev.twitter.com/streaming/public)
-and [node twitter API client](https://github.com/desmondmorris/node-twitter).
+and [node `twit twitter API client](https://github.com/ttezel/twit).
--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hello')
+var Twit = require('twit')
+
+var args = process.argv.slice(2);
+
+if (args.length !== 4) {
+    console.log('problem with arguments' +
+        'Expect: consumer_key,consumer_secret, access_token,access_token_secret ')
+    process.exit(1)
+}
+
+const config = {
+    consumer_key: args[0], consumer_secret: args[1],
+    access_token: args[2] , access_token_secret: args[3]
+}
+console.log('config:')
+console.log(config)
+
+
+var T = new Twit(config)
