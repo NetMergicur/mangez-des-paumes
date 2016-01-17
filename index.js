6a5bcd4a31133ee8b414ef7324f9bfb401afc175
AdrieanKhisbe <adriean.khisbe@live.fr>
2016-01-17 13:30:59 +0100

    feature: retweet #pomme

--- index.js
+++ index.js
@@ -20,15 +20,16 @@ var tb = new TwitBot(config)
 
 // https://twitter.com/search?f=tweets&q=%23touchepasauxguignols%20OR%20%23lesguignols%20from%3A%E2%80%8FSauvonsGuignols%20since%3A2015-08-04%20until%3A2016-01-04&src=typd
 // tb.T.get('search/tweets', { q: '', count: 100 }, (err,data)=>console.log('>>>%j',data))
-tb.T.get('search/tweets',
-    { q: '#touchepasauxguignols #lesguignols from:SauvonsGuignols since:2015-08-03 until:2015-08-05', count: 3 },
+/*
+tb.T.get('search/tweets', { q: '#touchepasauxguignols #lesguignols from:SauvonsGuignols since:2015-08-03 until:2015-08-05', count: 1},
     (err, data) => {
         if(err) return console.error('FAILED: %j', err)
-
-        console.log('>>>retrieved data: ', data)
         const id = data.search_metadata.max_id
         console.log('targerting: %d', id)
         tb.postRetweet(id)
     })
-
-tb.watch({track: '#NecMergitur'}, (tweet) => console.log(tweet))
+//*/
+console.log('starting to watch bash to keyword #pomme')
+tb.watch({track: '#pomme'}, (tweet) => {
+    tb.postRetweet(tweet.id_str)
+})
