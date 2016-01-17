a193ffcd70d4672af1fda1b1ccb927c83955a122
AdrieanKhisbe <adriean.khisbe@live.fr>
2016-01-17 11:10:11 +0100

    refactor: extract lib

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var Twit = require('twit')
-var Fs = require('fs')
+const TwitBot = require('./lib/twit-bot')
 
 var args = process.argv.slice(2);
 
@@ -13,40 +12,9 @@ const config = {
     consumer_key: args[0], consumer_secret: args[1],
     access_token: args[2], access_token_secret: args[3]
 }
-console.log('config:')
-console.log(config)
+console.log('Launching config bot with following config:\n%s', config)
 
+var tb = new TwitBot(config)
 
-var T = new Twit(config)
-
-function postTweet(status) {
-    T.post('statuses/update', {status: status}, function (err, data, response) {
-        if (err) console.err('Dawn it, it failed:\n%s', err)
-        console.log('Successful post of twitt: %s', status)
-    })
-}
-
-function postImage(path, status) {
-    var b64content = Fs.readFileSync(path, {encoding: 'base64'})
-
-    T.post('media/upload', {media_data: b64content}, function (err, data, response) {
-
-        if (err) return console.error('Uploading img failed')
-
-        // now we can reference the media and post a tweet (media will attach to the tweet)
-        var mediaIdStr = data.media_id_string
-        var params = {status: status, media_ids: [mediaIdStr]}
-
-        T.post('statuses/update', params, function (err, data, response) {
-            if (err) console.error('Post twit a échoué failed')
-            else console.log('Post twit réussi')
-        })
-    })
-}
-
-function watch(pattern, callback) {
-    var stream = T.stream('statuses/filter', pattern)
-    stream.on('tweet', callback)
-}
-
-watch({track: '#NecMergitur'}, (tweet) => console.log(tweet))
+tb.postImage("I'm Baaaaaaaaaack", 'images/guignols-im-back.jpg')
+tb.watch({track: '#NecMergitur'}, (tweet) => console.log(tweet))
