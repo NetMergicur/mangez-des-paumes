984ede8c768706e2686654f55e6bc172d9ad591d
AdrieanKhisbe <adriean.khisbe@live.fr>
2016-01-17 12:35:58 +0100

    dev: retweet in progress

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const TwitBot = require('./lib/twit-bot')
 
 var args = process.argv.slice(2);
@@ -16,5 +18,17 @@ console.log('Launching config bot with following config:\n%s', config)
 
 var tb = new TwitBot(config)
 
-tb.postImage("I'm Baaaaaaaaaack", 'images/guignols-im-back.jpg')
+// https://twitter.com/search?f=tweets&q=%23touchepasauxguignols%20OR%20%23lesguignols%20from%3A%E2%80%8FSauvonsGuignols%20since%3A2015-08-04%20until%3A2016-01-04&src=typd
+// tb.T.get('search/tweets', { q: '', count: 100 }, (err,data)=>console.log('>>>%j',data))
+tb.T.get('search/tweets',
+    { q: '#touchepasauxguignols #lesguignols from:SauvonsGuignols since:2015-08-03 until:2015-08-05', count: 3 },
+    (err, data) => {
+        if(err) return console.error('FAILED: %j', err)
+
+        console.log('>>>retrieved data: ', data)
+        const id = data.search_metadata.max_id
+        console.log('targerting: %d', id)
+        tb.postRetweet(id)
+    })
+
 tb.watch({track: '#NecMergitur'}, (tweet) => console.log(tweet))
